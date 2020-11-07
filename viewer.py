66a26cf40722b0a351bd2861ffefc9c3c2f71a82
Amanda Zhang <amanda.zhang25@gmail.com>
2020-11-06 23:10:33 -0800

    Loaded tickets into list

--- viewer.py
+++ viewer.py
@@ -1,10 +1,24 @@
 from flask import Flask, render_template
+import requests
+import os
+from dotenv import load_dotenv, find_dotenv
+
 app = Flask(__name__)
+load_dotenv(find_dotenv())
+zendesk_username = os.environ['ZENDESK_USERNAME']
+zendesk_password = os.environ['ZENDESK_PASSWORD']
 
-@app.route("/")
-def hello_world():
-    return render_template("index.jinja2")
+@app.route('/')
+def ticket_list():
+    tickets = []
+    url = 'https://azic.zendesk.com/api/v2/tickets.json'
+    while url:
+        r = requests.get(url, auth=(zendesk_username, zendesk_password))
+        response_json = r.json()
+        tickets += response_json['tickets']
+        url = response_json['next_page']
+    return render_template('index.jinja2', tickets=tickets)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
 
