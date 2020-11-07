26c51206b76f82e3a40f70453456c31805acd91d
Amanda Zhang <amanda.zhang25@gmail.com>
2020-11-06 21:51:51 -0800

    Served demo HTML page

--- viewer.py
+++ viewer.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return 'Hello, World!'
+    return render_template("index.jinja2")
 
 if __name__ == "__main__":
     app.run(debug=True)
